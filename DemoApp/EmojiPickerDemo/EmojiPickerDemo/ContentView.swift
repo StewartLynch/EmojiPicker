1126c941c8924a60b2b5ee86d617bdced5e708fe
Stewart Lynch <slynch@createchsol.com>
2025-06-29 19:09:18 -0700

    Added all Project files for demo

--- DemoApp/EmojiPickerDemo/EmojiPickerDemo/ContentView.swift
+++ DemoApp/EmojiPickerDemo/EmojiPickerDemo/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //----------------------------------------------
 // Original project: EmojiPickerDemo
-// by  Stewart Lynch on 2025-06-29
+// by  Stewart Lynch on 2025-06-22
 //
 // Follow me on Mastodon: https://iosdev.space/@StewartLynch
 // Follow me on Threads: https://www.threads.net/@stewartlynch
@@ -16,16 +16,149 @@
 
 
 import SwiftUI
+import EmojiPicker
 
 struct ContentView: View {
+    @State private var allCategories = "😀"
+    @State private var singleCategory = "🇨🇦"
+    @State private var mixedCategories = "👓"
+    @State private var searchTerm = "❤️"
+    // Presentation Triggers
+    @State private var showAll = false
+    @State private var showSingle = false
+    @State private var showMixed = false
+    @State private var showSearchTerm = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    HStack {
+                        Text(
+"""
+EmojiPickerView(
+   selectedEmoji: $allCategories
+)
+"""
+                        )
+                        .font(.system(size: 12))
+                        .fixedSize(horizontal: true, vertical: false)
+                        .foregroundStyle(.secondary)
+                        Button {
+                            showAll = true
+                        } label: {
+                            Text(allCategories)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.plain)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .sheet(isPresented: $showAll) {
+                            EmojiPickerView(
+                               selectedEmoji: $allCategories
+                            )
+                        }
+                    }
+                } header: {
+                    Text("All Categories Available")
+                }
+                Section {
+                    HStack{
+                        Text(
+"""
+EmojiPickerView(
+    selectedEmoji: $singleCategory,
+    limitedCategories: [.flags]
+)
+""")
+                        .font(.system(size: 12))
+                        .foregroundStyle(.secondary)
+                        .fixedSize(horizontal: true, vertical: false)
+                        Spacer()
+                        Button {
+                            showSingle = true
+                        } label: {
+                            Text(singleCategory)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.plain)
+                        .popover(isPresented: $showSingle) {
+                            EmojiPickerView(
+                                selectedEmoji: $singleCategory,
+                                limitedCategories: [.flags]
+                            )
+                            .frame(width: 300, height: 300)
+                            .presentationCompactAdaptation(.popover)
+                        }
+                    }
+                } header: {
+                    Text("Single Category Available")
+                }
+                Section {
+                    HStack{
+                        Text(
+"""
+EmojiPickerView(
+    selectedEmoji: $mixedCategories,
+    limitedCategories: [.symbols, .objects]
+)
+""")
+                        .font(.system(size: 10))
+                        .foregroundStyle(.secondary)
+                        .fixedSize(horizontal: true, vertical: false)
+                        Spacer()
+                        Button {
+                            showMixed = true
+                        } label: {
+                            Text(mixedCategories)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.plain)
+                        .sheet(isPresented: $showMixed) {
+                            EmojiPickerView(
+                                selectedEmoji: $mixedCategories,
+                                limitedCategories: [.symbols, .objects]
+                            )
+                            .presentationDetents([.medium])
+                        }
+                    }
+                } header: {
+                    Text("Multiple Categories Only")
+                }
+                Section {
+                    HStack{
+                        Text(
+"""
+EmojiPickerView(
+    selectedEmoji: $searchTerm,
+    searchTerm: "love"
+)
+""")
+                        .font(.system(size: 12))
+                        .foregroundStyle(.secondary)
+                        .fixedSize(horizontal: true, vertical: false)
+                        Spacer()
+                        Button {
+                            showSearchTerm = true
+                        } label: {
+                            Text(searchTerm)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.plain)
+                        .popover(isPresented: $showSearchTerm) {
+                            EmojiPickerView(
+                                selectedEmoji: $searchTerm,
+                                searchTerm: "love"
+                            )
+                            .frame(width: 300, height: 300)
+                            .presentationCompactAdaptation(.popover)
+                        }
+                    }
+                } header: {
+                    Text("Search Term only")
+                }
+            }
+            .navigationTitle("Emoji Picker")
         }
-        .padding()
     }
 }
 
